638a0175b3e6fb4e994f5ced10a14b5baf331f72
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-26 22:17:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5173', // Vite (optional)
-  'https://your-deployed-frontend.com', // Add your prod URL here
+  'https://notes-app-client-mtvc.onrender.com', // Add your prod URL here
 ];
 
 // ✅ Middleware

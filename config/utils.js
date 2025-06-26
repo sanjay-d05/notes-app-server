60fa16c1d6072e999eca2765246c45d49671c5bb
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-26 22:47:25 +0530

    Update utils.js

--- config/utils.js
+++ config/utils.js
@@ -7,7 +7,7 @@ export const generateToken = async(userId ,  res) => {
         maxAge:1*24*60*60*1000,
         httpOnly:true,
         sameSite:"none",
-        secure:"true"
+        secure:true
     }) ;
 
     return token;

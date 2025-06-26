7e4dfef0336d345e0aeb4fedbca4ee885ebaec76
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-26 22:36:16 +0530

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -57,15 +57,20 @@ export const login = async(req,res) => {
     }
 };
 
-export const logout = async(req,res) => {
-    try {
-        res.cookie('notesAccessToken', '' , {maxAge:0} );
-        return res.status(200).json({success:true,message:"Logged out successfully"});
-    } catch (error) {
-        console.log('Error in logout route' , error);
-        return res.status(500).json({success:false,message:'Internal Server Error'});
-    }
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('notesAccessToken', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none'
+    });
+    return res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.log('Error in logout route', error);
+    return res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
 };
+
 
 export const checkAuth = async(req,res) => {
     try {
